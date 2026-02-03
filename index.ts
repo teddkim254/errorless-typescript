f84b7989bc8ffc4862d269d50d039527f9bd6fd5
Tedd Kim <teddkimnskills@gmail.com>
2026-02-03 18:28:08 +0300

    refactor(works): comment flow

--- index.ts
+++ index.ts
@@ -1,30 +1,52 @@
+/**
+ * @param None
+ * @returns string of intro message
+ */
 function useMagnifyingGlass():string{
   return 'I will use my magnifying glass.'
 }
 
+
+/**
+ * @param None
+ * @returns either 1 or 2 randomly to represent the culprit
+ */
 function determineCulprit(){
   return Math.floor(Math.random()*2+1)
 }
 
+
+/**
+ * 
+ * @param numberOfClues 
+ * @param clue1 
+ * @param clue2 
+ * @param suspect1 
+ * @param suspect2 
+ * @returns culpritNumber - the number representing the culprit
+ */
 function doSleuthing(numberOfClues:number, clue1:string, clue2:string, suspect1:string, suspect2:string):number{
   console.log('I am a famous detective and I will solve the crime.');
-  let unnecessaryVariable = 'Why is this here?'
+
+  // Using the magnifying glass is important to sleuthing
+  let unnecessaryVariable = 'Why is this here?' //all fun and code!
   unnecessaryVariable=useMagnifyingGlass();
+  console.log(unnecessaryVariable);
 
   console.log('Now I consider the first clue: ',clue1);
   console.log('Now I consider the second clue: ',clue2);
 
   let culpritNumber = determineCulprit();
-  console.log('Now, I will return to you the culprit. There but for the grace of God go we.' );
+  console.log('Now, I will return to you the culprit. There, but for the grace of God go we got...' );
   
-    if(culpritNumber == 1){
-    console.log(suspect1);
+  if(culpritNumber == 1){
+    console.log(   suspect1,'!');
   }
   else if(culpritNumber == 2){
-    console.log(suspect2);
+    console.log(   suspect2,'!');
   }
   else{
-   console.log("I couldn't figure out who drank the priceless milk. :( :(");
+   console.log("I couldn't figure out who drank the priceless milk. :( :(");//kinda useless but fun!
   }
 
   return culpritNumber;
