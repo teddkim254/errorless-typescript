46628e724e7ca231bcedf81d9e40837049d1a846
Tedd Kim <teddkimnskills@gmail.com>
2026-02-03 17:29:27 +0300

    chore(works): initial debug

--- index.ts
+++ index.ts
@@ -1,29 +1,40 @@
 function useMagnifyingGlass():string{
-  console.log('I will use my magnifying glass.')
+  return 'I will use my magnifying glass.'
 }
 
 function determineCulprit(){
   return Math.floor(Math.random()*2+1)
 }
 
-function doSleuthing(numberOfClues:number, clue1:string, clue2:string, suspect1:string, suspect2:number):void{
+function doSleuthing(numberOfClues:number, clue1:string, clue2:string, suspect1:string, suspect2:string):number{
   console.log('I am a famous detective and I will solve the crime.');
   let unnecessaryVariable = 'Why is this here?'
   unnecessaryVariable=useMagnifyingGlass();
 
   console.log('Now I consider the first clue: ',clue1);
-  console.log('Now I consider the second clue: ',clue1);
+  console.log('Now I consider the second clue: ',clue2);
 
-  let culpritNumber:string = determineCulprit();
+  let culpritNumber = determineCulprit();
   console.log('Now, I will return to you the culprit. There but for the grace of God go we.' );
-  if(culpritNumber == 1) {return(suspect1)}
-  if(culpritNumber == 2) {return(suspect2)}
   
-  return "I couldn't figure out who drank the priceless milk. :( :("
+    if(culpritNumber == 1){
+    console.log(suspect1);
+  }
+  else if(culpritNumber == 2){
+    console.log(suspect2);
+  }
+  else{
+   console.log("I couldn't figure out who drank the priceless milk. :( :(");
+  }
+
+  return culpritNumber;
+
 }
+
+
 
 let answer=3;
 
-answer=doSleuthing('2', 'The parrot heard everything!', 'All the doors and windows were shut from the INSIDE.', 'Burglar Bob', 'Saint Sam')
+answer=doSleuthing(2, 'The parrot heard everything!', 'All the doors and windows were shut from the INSIDE.', 'Burglar Bob', 'Saint Sam')
 
 console.log('The culprit was none other than ', answer, '!');
